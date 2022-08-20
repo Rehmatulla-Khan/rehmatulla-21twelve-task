d352cddbbd3d8fbcf428529bc5f4072f5f430261
coder-mezab <k.rehmatulla22@gmail.com>
2022-08-20 16:17:15 +0530

    🐛 responsive issue in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import UserCard from "../../component/card/UserCard";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 gap-2 mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center">
+    <div className="grid grid-cols-1 gap-2 mt-16 md:grid-cols-2 lg:grid-cols-3 place-items-center">
       <UserCard />
     </div>
   );
